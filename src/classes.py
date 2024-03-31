17ef6955d4cda6d38d1cf4e5cb04fd0f32d5649a
Rockfact2017 <160278344+Rockfact2017@users.noreply.github.com>
2024-03-31 23:28:57 +0300

    Initial commit

---
+++ src/classes.py
@@ -0,0 +1,26 @@
+class Category:
+    name: str
+    description: str
+    products: list
+    total_categories: int = 0
+    total_unique_products: int = 0
+
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.products = products
+        Category.total_categories += 1
+        Category.total_unique_products += len(set(products))
+
+
+class Product:
+    name: str
+    description: str
+    price: float
+    quantity_in_stock: int
+
+    def __init__(self, name, description, price, quantity_in_stock):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity_in_stock = quantity_in_stock
