95f710ef4564abdb3af75f3e6a568b419b3068f0
Rockfact2017 <160278344+Rockfact2017@users.noreply.github.com>
2024-05-17 01:18:54 +0300

    Homework 15.2

--- src/classes.py
+++ src/classes.py
@@ -1,51 +1,106 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
-class Category:
+class LoggingMixin:
+    """
+    Миксин для логирования создания объектов.
+    Выводит в консоль сообщение о создании объекта.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        print(f"{self.__repr__()} создан.")  # Логирование создания объекта
+
+    def __repr__(self):
+        """
+        Возвращает строковое представление объекта с его атрибутами.
+        """
+        attributes = ', '.join([f"{key}={value}" for key, value in self.__dict__.items()])
+        return f"{self.__class__.__name__}({attributes})"
+
+class AbstractProduct(ABC):
+    """
+    Абстрактный класс для продуктов.
+    Определяет основные свойства и методы, которые должны быть реализованы в подклассах.
+    """
     name: str
     description: str
-    products: list
-    total_categories: int = 0
-    total_unique_products: int = 0
+    price: float
+    quantity_in_stock: int
 
-    def __init__(self, name, description, products):
+    @abstractmethod
+    def __init__(self, name, description, price, quantity_in_stock):
+        """
+        Инициализирует объект продукта.
+        """
         self.name = name
         self.description = description
-        self.__products = products
-        Category.total_categories += 1
-        Category.total_unique_products += len(set(products))
+        self._price = price
+        self.quantity_in_stock = quantity_in_stock
 
-    def add_product(self, product):
-        if not isinstance(product, Product) and not issubclass(type(product), Product):
-            raise TypeError("В категорию можно добавлять только объекты класса Product и его наследников.")
-
-        self.__products.append(product)
+    @abstractmethod
+    def __str__(self):
+        """
+        Возвращает строковое представление продукта.
+        """
+        pass
 
     @property
-    def list_of_products(self):
-        return "\n".join(
-            [f"{product.name}, {product.price} руб. Остаток: {product.quantity_in_stock} шт." for product in
-             self.__products])
+    def price(self):
+        """
+        Возвращает цену продукта.
+        """
+        return self._price
 
-    def __str__(self):
-        return f"{self.name}, количество продуктов: {len(self.__products)} шт."
+    @price.setter
+    def price(self, new_price):
+        """
+        Устанавливает новую цену продукта.
+        Если цена понижается, запрашивает подтверждение.
+        """
+        if new_price <= 0:
+            print("Цена введена некорректно.")  # Проверка корректности цены
+        else:
+            if self._price > new_price:
+                confirmation = input("Цена товара понижается. Подтвердите действие (y/n): ")
+                if confirmation == "y":
+                    self._price = new_price
+                else:
+                    print("Действие отменено.")  # Отмена действия при отказе
+            else:
+                self._price = new_price
 
-    def __len__(self):
-        return len(self.__products)
+    def __call__(self):
+        """
+        Возвращает цену продукта при вызове объекта.
+        """
+        return self._price
 
+    @abstractmethod
+    def __add__(self, other):
+        """
+        Осуществляет сложение двух продуктов одного типа.
+        """
+        pass
 
-class Product(ABC):
+class Product(LoggingMixin, AbstractProduct):
+    """
+    Класс, представляющий общий продукт.
+    """
     def __init__(self, name, description, price, quantity_in_stock):
-        self.name = name
-        self.description = description
-        self.__price = price
-        self.quantity_in_stock = quantity_in_stock
+        super().__init__(name, description, price, quantity_in_stock)
 
     @classmethod
     def create_product(cls, name, description, price, quantity_in_stock):
+        """
+        Создает новый продукт.
+        """
         return cls(name, description, price, quantity_in_stock)
 
     @classmethod
     def add_product(cls, name, description, price, quantity_in_stock, products):
+        """
+        Добавляет продукт к списку продуктов, увеличивая количество,
+        если продукт с таким именем уже существует.
+        """
         for product in products:
             if product.name == name:
                 product.quantity_in_stock += quantity_in_stock
@@ -54,34 +109,16 @@ class Product(ABC):
                 return product
         return cls(name, description, price, quantity_in_stock)
 
-    @property
-    def price(self):
-        return self.__price
-
-    @price.setter
-    def price(self, new_price):
-        if new_price <= 0:
-            print("Цена введена некорректно.")
-        else:
-            if self.__price > new_price:
-                confirmation = input("Цена товара понижается. Подтвердите действие (y/n): ")
-                if confirmation == "y":
-                    self.__price = new_price
-                else:
-                    print("Действие отменено.")
-            else:
-                self.__price = new_price
-
-            if self.__price < self.old_price:
-                self.old_price = self.__price
-
-    def __call__(self):
-        return self.__price
-
     def __str__(self):
-        return f"{self.name}, {self.__price} руб. Остаток: {self.quantity_in_stock} шт."
+        """
+        Возвращает строковое представление продукта.
+        """
+        return f"{self.name}, {self._price} руб. Остаток: {self.quantity_in_stock} шт."
 
     def __add__(self, other):
+        """
+        Складывает количество на складе двух продуктов одного типа.
+        """
         if not isinstance(other, Product):
             raise TypeError("Можно складывать только объекты класса Product и его наследников.")
 
@@ -90,21 +127,10 @@ class Product(ABC):
 
         return self() * self.quantity_in_stock + other() * other.quantity_in_stock
 
-
-class Printable:
-    def __repr__(self):
-        return f"{self.__class__.__name__}({', '.join([f'{key}={value}' for key, value in self.__dict__.items()])})"
-
-
-class ProductMixin(Printable):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        print(f"Создан объект {self.__class__.__name__} со следующими атрибутами:")
-        for key, value in self.__dict__.items():
-            print(f"{key}={value}")
-
-
-class Smartphone(Product, ProductMixin):
+class Smartphone(Product):
+    """
+    Класс, представляющий смартфон.
+    """
     def __init__(self, name, description, price, quantity_in_stock, performance, model, internal_memory, color):
         super().__init__(name, description, price, quantity_in_stock)
         self.performance = performance
@@ -113,13 +139,18 @@ class Smartphone(Product, ProductMixin):
         self.color = color
 
     def __add__(self, other):
+        """
+        Складывает количество на складе двух смартфонов.
+        """
         if not isinstance(other, Smartphone):
             raise TypeError("Можно складывать только объекты класса Smartphone.")
 
         return self() * self.quantity_in_stock + other() * other.quantity_in_stock
 
-
-class Grass(Product, ProductMixin):
+class Grass(Product):
+    """
+    Класс, представляющий газонную траву.
+    """
     def __init__(self, name, description, price, quantity_in_stock, country_of_origin, germination_period, color):
         super().__init__(name, description, price, quantity_in_stock)
         self.country_of_origin = country_of_origin
@@ -127,10 +158,64 @@ class Grass(Product, ProductMixin):
         self.color = color
 
     def __add__(self, other):
+        """
+        Складывает количество на складе двух видов газонной травы.
+        """
         if not isinstance(other, Grass):
             raise TypeError("Можно складывать только объекты класса Grass.")
 
         return self() * self.quantity_in_stock + other() * other.quantity_in_stock
+
+class Category:
+    """
+    Класс, представляющий категорию продуктов.
+    """
+    name: str
+    description: str
+    products: list
+    total_categories: int = 0
+    total_unique_products: int = 0
+
+    def __init__(self, name, description, products):
+        """
+        Инициализирует объект категории.
+        """
+        self.name = name
+        self.description = description
+        self.__products = products
+        Category.total_categories += 1
+        Category.total_unique_products += len(set(products))
+
+    def add_product(self, product):
+        """
+        Добавляет продукт в категорию.
+        """
+        if not isinstance(product, Product) and not issubclass(type(product), Product):
+            raise TypeError("В категорию можно добавлять только объекты класса Product и его наследников.")
+
+        self.__products.append(product)
+
+    @property
+    def list_of_products(self):
+        """
+        Возвращает список продуктов в категории в виде строки.
+        """
+        return "\n".join(
+            [f"{product.name}, {product.price} руб. Остаток: {product.quantity_in_stock} шт." for product in
+             self.__products])
+
+    def __str__(self):
+        """
+        Возвращает строковое представление категории.
+        """
+        return f"{self.name}, количество продуктов: {len(self.__products)} шт."
+
+    def __len__(self):
+        """
+        Возвращает количество продуктов в категории.
+        """
+        return len(self.__products)
+
 
 
 
